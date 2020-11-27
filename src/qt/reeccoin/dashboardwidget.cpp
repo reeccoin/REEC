8a83fcce710cc56eb4224fa70abe32196352bde2
reeccoin <69335017+reeccoin@users.noreply.github.com>
2020-11-27 13:30:14 +0800

    chart color updated.

--- src/qt/reeccoin/dashboardwidget.cpp
+++ src/qt/reeccoin/dashboardwidget.cpp
@@ -667,8 +667,8 @@ void DashboardWidget::onChartRefreshed()
     // init sets
     set0 = new QBarSet(CURRENCY_UNIT.c_str());
     set1 = new QBarSet("z" + QString(CURRENCY_UNIT.c_str()));
-    set0->setColor(QColor(92,75,125));
-    set1->setColor(QColor(176,136,255));
+    set0->setColor(QColor(15,91,153));
+    set1->setColor(QColor(160,211,255));
 
     if (!series) {
         series = new QBarSeries();
